5ff9344b1f52837a2ac1151eed596ac6a34bf551
Furkan İzgi <76229401+Furkan-izgi@users.noreply.github.com>
2021-09-10 23:04:36 +0300

    İlk güncelleme!
    Merhaba! Bu güncelleme, En Uygun Araç'a aşağıda belirttiğimiz eklemeleri, kod ve arayüz iyileştirmelerini kapsamaktadır:
    * Açık ve Koyu tema eklendi!
    * Siz özelliklere daha kolay ulaşın diye alt tarafa özelliklerin kısayolları eklendi!
    * Artık bütün modellerin resimleri En Uygun Araç'ta mevcut!
    * Kaynak koddaki bazı gereksiz kodlar temizlendi ve iyileştirmeler yapıldı.
    
    Desteklemek için En Uygun Araç'ı paylaşmayı ve yıldız atmayı unutmayın!

--- frontend.py
+++ frontend.py
@@ -4,13 +4,7 @@ from PyQt5.QtWidgets import QAction, QApplication, QComboBox, QLabel, QMainWindo
 import sys
 import backend
 import webbrowser
-
-class WinTable(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Hakkında")
-        self.setGeometry(100, 100, 400, 400) 
-        
+      
 class Window(QMainWindow):
         
     def __init__(self):
@@ -31,7 +25,7 @@ class Window(QMainWindow):
         
     def getPicture(self):
         self.model_index = self.combobox_model.currentIndex()
-        self.pic = QPixmap("assets\\" + self.marka_dicts[self.combobox_marka.currentText()] + "\\"+ str(self.model_index) +".png").scaled(360, 360, QtCore.Qt.KeepAspectRatio)
+        self.pic = QPixmap("assets\\" + self.marka_dicts[self.combobox_marka.currentText()] + "\\"+ str(self.model_index) +".webp").scaled(310, 310, QtCore.Qt.KeepAspectRatio)
              
     def getModel(self,model_name):
         model_name = self.combobox_model.currentText()
@@ -52,6 +46,25 @@ class Window(QMainWindow):
             Window.getPicture(self)
             self.pic_lbl.setPixmap(self.pic)
             
+    def whiteTheme(self):
+        self.variables  = [self.lbl_marka_info,self.lbl_model_info,self.lbl_2_fiyat,self.lbl_title,self.lbl_marka,self.lbl_model,self.marka_btn,self.model_btn,self.lbl_sifir_fiyat,self.combobox_marka,self.combobox_model,self.about]
+               
+        self.setStyleSheet("background-color: #dee3e3;")
+        
+        for variable in self.variables:#Bütün labellar, butonlar, comboboxların text colorları değiştirildi!
+            variable.setStyleSheet("color: black;")
+            
+        self.menubar.setStyleSheet("background-color:#dee3e3; color:black;")
+   
+    def grayTheme(self):       
+        self.variables  = [self.lbl_marka_info,self.lbl_model_info,self.lbl_2_fiyat,self.lbl_title,self.lbl_marka,self.lbl_model,self.marka_btn,self.model_btn,self.lbl_sifir_fiyat,self.combobox_marka,self.combobox_model,self.about]
+        
+        self.setStyleSheet("background-color: #424751;")
+        
+        for variable in self.variables:
+            variable.setStyleSheet("color:white;")
+            
+        self.menubar.setStyleSheet("background-color: #424751; color:white;")
             
     def getMarka(self,marka_name):
         marka_name = self.combobox_marka.currentText()
@@ -66,16 +79,15 @@ class Window(QMainWindow):
             self.model_btn.setEnabled(True)
             
     def picOn(self):
-        self.pic_lbl.resize(330,200)
+        self.pic_lbl.resize(350,200)
     
     def picOff(self):
         self.pic_lbl.resize(0,0)
     
     def initUI(self):
-        self.setGeometry(100, 100, 600, 370)
+        self.setGeometry(100, 100, 610, 400)
         self.setWindowTitle("En Uygun Araç")
         self.setWindowIcon(QIcon("assets\\app.svg"))
-        self.setStyleSheet("background-color: gray;")
         #################################################################################################################
         self.marka_dicts= {"Audi":"audi", "Bmw":"bmw", "Citroen":"citroen", "Dacia":"dacia","Fiat":"fiat", "Ford":"ford","Honda":"honda","Hyundai":"hyundai",
                            "Isuzu":"isuzu", "Jaguar":"jaguar", "Jeep":"jeep","Kia":"kia","Lada":"lada", "Land Rover":"land-rover","Lexus":"lexus", 
@@ -83,8 +95,13 @@ class Window(QMainWindow):
                            "Opel":"opel", "Peugeot":"peugeot","Porsche":"porsche","Renault":"renault", "Seat":"seat","Skoda":"skoda","Smart":"smart", 
                            "Ssangyong":"ssangyong", "Subaru":"subaru","Suzuki":"suzuki", "Toyota":"toyota", "Volkswagen":"volkswagen", "Volvo":"volvo"}
         #################################################################################################################
+        self.logo = QPixmap("assets/app.svg").scaled(20, 20, QtCore.Qt.KeepAspectRatio)
+        self.lbl_logo = QLabel(self)
+        self.lbl_logo.setPixmap(self.logo)
+        self.lbl_logo.move(240, 15)
+        
         self.lbl_title = QLabel("En Uygun Araç", self)
-        self.lbl_title.move(260, 10)
+        self.lbl_title.move(265, 15)
         
         self.lbl_marka = QLabel("Marka", self)
         self.lbl_marka.move(130, 50)
@@ -125,37 +142,78 @@ class Window(QMainWindow):
         self.car_link.setOpenExternalLinks(True)
         
         self.pic_lbl = QLabel(self)
-        self.pic_lbl.move(270, 180)
-        self.pic_lbl.resize(330,200)
+        self.pic_lbl.move(280, 180)
+        self.pic_lbl.resize(350,200)
         
         self.menubar = self.menuBar()
         
         self.settings = self.menubar.addMenu("&Ayarlar")
-        self.pic_on = QAction("Resimleri &Aç",self)
-        self.pic_on.setShortcut("Ctrl+O")
-        self.pic_on.setToolTip("Araba resimlerini açar.")
-        self.pic_on.triggered.connect(lambda: Window.picOn(self))
-        
-        self.pic_off = QAction("Resimleri &Kapat",self)
-        self.pic_off.setShortcut("Ctrl+C")
-        self.pic_on.setToolTip("Araba resimlerini kapatır.")
-        self.pic_off.triggered.connect(lambda: Window.picOff(self))
-        
-        self.settings.addAction(self.pic_on)
-        self.settings.addAction(self.pic_off)
-        
+        self.pic_settings = self.settings.addMenu("&Görüntü")
+        self.menubar.addSeparator()
+        self.theme = self.settings.addMenu("&Temalar")
         self.about = self.menubar.addMenu("&Hakkında")
         
-        self.github = QAction("&Github", self)
-        self.github.setShortcut("Ctrl+G")
+        self.toolbar = QtWidgets.QToolBar("Example")
+        self.toolbar.setMovable(False)
+        self.addToolBar(QtCore.Qt.BottomToolBarArea, self.toolbar)
+        
+        self.pic_on = QAction(QIcon("assets/icons/pic_on.png"), "Resimleri &Aç", self)
+        self.pic_on.setShortcut("Ctrl+O")
+        self.pic_on.triggered.connect(lambda: Window.picOn(self))
+        
+        self.pic_off = QAction(QIcon("assets/icons/pic_off.png"), "Resimleri &Kapat", self)
+        self.pic_off.setShortcut("Ctrl+C")
+        self.pic_off.triggered.connect(lambda: Window.picOff(self))
+        
+        self.gray_theme = QAction(QIcon("assets/icons/gray.png"), "&Gri Tema", self)
+        self.gray_theme.triggered.connect(lambda: Window.grayTheme(self))
+        
+        self.white_theme = QAction(QIcon("assets/icons/white.png"), "&Beyaz Tema", self)
+        self.white_theme.triggered.connect(lambda: Window.whiteTheme(self))
+        
+        self.github = QAction(QIcon("assets/icons/github.png"), "&Github", self)
         self.github.triggered.connect(lambda: webbrowser.open_new('https://github.com/Furkan-izgi'))
         
-        self.linkedin = QAction("&Linkedin", self)
-        self.linkedin.setShortcut("Ctrl+L")
+        self.linkedin = QAction(QIcon("assets/icons/linkedin.png"), "&Linkedin", self)
         self.linkedin.triggered.connect(lambda: webbrowser.open_new('https://www.linkedin.com/in/furkan-izgi/'))
-        
+        #########################################################################################################
         self.about.addAction(self.github)
         self.about.addAction(self.linkedin)
+        
+        self.pic_settings.addAction(self.pic_on)
+        self.pic_settings.addAction(self.pic_off)
+        
+        self.toolbar.addAction("").setEnabled(False)#Toolbardaki fonksiyonların arasına boşluk eklemek için kullanıldı.
+        self.toolbar.addAction("").setEnabled(False)
+        self.toolbar.addAction("").setEnabled(False)
+        
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.pic_on)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.pic_off)
+        self.toolbar.addSeparator()
+        
+        self.toolbar.addAction("").setEnabled(False)
+        self.toolbar.addAction("").setEnabled(False)
+        
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.gray_theme)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.white_theme)
+        self.toolbar.addSeparator()
+        
+        self.toolbar.addAction("").setEnabled(False)
+        self.toolbar.addAction("").setEnabled(False)
+        
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.github)
+        self.toolbar.addSeparator()
+        self.toolbar.addAction(self.linkedin)
+        self.toolbar.addSeparator()
+        
+        self.theme.addAction(self.white_theme)
+        self.theme.addAction(self.gray_theme)
+        
         
         ########################################################################
         self.combobox_marka = QComboBox(self)
@@ -173,4 +231,5 @@ class Window(QMainWindow):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = Window()
+    win.grayTheme()
     sys.exit(app.exec())
